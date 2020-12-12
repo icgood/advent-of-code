65dcfeb5b943baea709b3dcd4fb6f73a6631e892
Ian Good <ian@icgood.net>
2020-12-12 14:23:42 -0500

    Add template for new day solutions

---
+++ day_base/day_template/sol.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "day_base/day_result.h"
+
+void day_result_compute(char *arg, day_result *res, FILE *in) {
+}
+
+void day_answers_provide(day_arguments *args, day_answers *answers) {
+	*answers = (day_answers) {
+		{0, 0},
+		{0, 0},
+	};
+}
